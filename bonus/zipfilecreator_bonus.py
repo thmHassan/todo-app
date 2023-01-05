d552f2c56a40ed36225335d2ec9fa5b9bbd59fc9
thmHassan <121732869+thmHassan@users.noreply.github.com>
2023-01-06 01:39:09 +0500

    zipcreator cli

---
+++ bonus/zipfilecreator_bonus.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+from zipcreator import make_archive
+
+label1 = sg.Text("Select Files to compress:" )
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose", key='files')
+
+
+label2 = sg.Text("Select destination folder:")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose", key='folder')
+compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output")
+
+window = sg.Window("File Compressor",
+                   layout=[[label1, input1, choose_button1],
+                           [label2, input2, choose_button2],
+                           [compress_button, output_label]])
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(value="Compression Completed")
+
+window.close()
